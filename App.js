18b4bfd3768c0b567b31a087d8c908f41d8a2be9
Umer Nasir <umernasirr@gmail.com>
2020-09-02 20:09:08 +0500

    Added Assets

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Screens
+import Welcome from './screens/Welcome';
+import Explore from './screens/Explore';
+import Login from './screens/Login';
+import Products from './screens/Products';
+import Settings from './screens/Settings';
+import Browse from './screens/Browse';
+
+import { theme } from './constants';
+const ScreenStack = createStackNavigator();
+const Screen = () => (
+	<ScreenStack.Navigator
+		screenOptions={{
+			headerStyle: {},
+			headerBackImage: () => <Image />,
+			headerBackTitle: () => null,
+			headerLeftContainerStyle: {},
+			headerRightContainerStyle: {},
+		}}
+	>
+		<ScreenStack.Screen name="Welcome" component={Welcome} />
+	</ScreenStack.Navigator>
+);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Screen />
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
---
+++ constants/mocks.js
@@ -0,0 +1,99 @@
+const categories = [
+	{
+		id: 1,
+		category: 'plants',
+		name: 'Plants',
+		tags: ['products', 'inspirations', 'shop'],
+		count: 100,
+		image: require('../assets/icons/plants.png'),
+	},
+
+	{
+		id: 2,
+		category: 'seeds',
+		name: 'Seeds',
+		tags: ['products', 'shop'],
+		count: 20,
+		image: require('../assets/icons/seeds.png'),
+	},
+
+	{
+		id: 3,
+		category: 'flowers',
+		name: 'Flowers',
+		tags: ['products', 'inspirations'],
+		count: 50,
+		image: require('../assets/icons/flowers.png'),
+	},
+	{
+		id: 4,
+		category: 'sprayers',
+		name: 'Sprayers',
+		tags: ['products', 'shop'],
+		count: 35,
+		image: require('../assets/icons/sprayers.png'),
+	},
+
+	{
+		id: 5,
+		category: 'pots',
+		name: 'Pots',
+		tags: ['products', 'inspirations'],
+		count: 35,
+		image: require('../assets/icons/pots.png'),
+	},
+
+	{
+		id: 6,
+		category: 'fertilizers',
+		name: 'Fertilizers',
+		tags: ['products', 'shop'],
+		count: 35,
+		image: require('../assets/icons/fertilizers.png'),
+	},
+];
+
+const products = [
+	{
+		id: 1,
+		name: '16 Best Plants That Thrive In Your Bedroom',
+		description:
+			'Bedrooms deserve to be decorated with lush greenery just like every other room in the house – but it can be tricky to find a plant that thrives here. Low light, high humidity and warm temperatures mean only certain houseplants will flourish.',
+		tags: ['Interior', '27 m²', 'Ideas'],
+		images: [
+			require('../assets/images/plants_1.png'),
+			require('../assets/images/plants_2.png'),
+			require('../assets/images/plants_3.png'),
+			// showing only 3 images, show +6 for the rest
+			require('../assets/images/plants_1.png'),
+			require('../assets/images/plants_2.png'),
+			require('../assets/images/plants_3.png'),
+			require('../assets/images/plants_1.png'),
+			require('../assets/images/plants_2.png'),
+			require('../assets/images/plants_3.png'),
+		],
+	},
+];
+
+const explore = [
+	//  images
+	require('../assets/images/explore_1.png'),
+	require('../assets/images/explore_2.png'),
+	require('../assets/images/explore_3.png'),
+	require('../assets/images/explore_4.png'),
+	require('../assets/images/explore_5.png'),
+	require('../assets/images/explore_6.png'),
+];
+
+const profile = {
+	username: 'Lorem Ipsum',
+	location: 'Lorem Ipsum Dolar',
+	email: 'Lorem@ipsum.com',
+	avatar: require('../assets/images/avatar.png'),
+	budget: 1000,
+	monthly_cap: 5000,
+	notifications: true,
+	newsletter: false,
+};
+
+export { categories, products, profile, explore };
